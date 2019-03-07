68466dfcb73fcc11de971283fc8ac54b246342b3
Jwngfu Brahma <open.jwngfu@gmail.com>
2019-03-08 01:05:02 +0530

    The Application is now a Nodejs App

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require('http');
+
+const PORT = process.env.PORT || 5000
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', function (req, res){
+    res.sendFile(__dirname, "public/index.html");
+});
+
+
+app.listen(PORT, () =>console.log(`Listening on ${ PORT }`));
