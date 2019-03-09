6c2e144341c01305989d4442568fa2d1818fd7f6
Jwngfu Brahma <open.jwngfu@gmail.com>
2019-03-10 00:37:08 +0530

    .gitignore file added

--- server.js
+++ server.js
@@ -2,15 +2,29 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const http = require('http');
+const config = require('./config.json')
 
 const PORT = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', function (req, res){
-    res.sendFile(__dirname, "public/login.html");
+    res.sendFile(__dirname + "/public/login.html");
 });
 
+app.route('/login')
+    .get(/*sessionChecker,*/ (req, res) => {
+        /*
+        res.render('login',{
+            title:'login',
+        });    */
+        res.sendFile(__dirname + "/public/login.html");
+    })
+    .post((req, res) => {
+        var username = req.body.username,
+            password = req.body.password,
+			usertype = req.body.usertype;
+    });
 
 
 //Start the server
