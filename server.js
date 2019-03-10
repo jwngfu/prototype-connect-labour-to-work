979c8393a255455a186db799d38175dc2a152e4c
Jwngfu Brahma <open.jwngfu@gmail.com>
2019-03-10 22:58:21 +0530

    Registration/Sign-Up implemented

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 const path = require('path');
 const http = require('http');
 const config = require('./config.json')
+const pg = require('pg')
+const pool = new pg.Pool(config.pg)
 
 const PORT = process.env.PORT || 5000
+
+pool.query("CREATE TABLE IF NOT EXISTS userData ( phoneNumber int PRIMARY KEY, userType int, name text, password text, address text, adharNumber int);", (err, res) => {
+    //console.log(err, res);
+    //pool.end();
+});
+
+
+
+// initialize body-parser to parse incoming parameters requests to req.body
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -21,9 +34,32 @@ app.route('/login')
         res.sendFile(__dirname + "/public/login.html");
     })
     .post((req, res) => {
-        var username = req.body.username,
+        var username = req.body.email;
+        var password = req.body.password;
+        console.log(username, password);
+        res.send("username : " + username);
+    });
+
+app.route('/register')
+    .get(/*sessionChecker,*/ (req, res) => {
+        /*
+        res.render('login',{
+            title:'login',
+        });    */
+        res.sendFile(__dirname + "/public/register.html");
+    })
+    .post((req, res) => {
+        var 
+            phoneNumber = req.body.phoneNumber,
+            userType = req.body.userType,    
+            name = req.body.name,
             password = req.body.password,
-			usertype = req.body.usertype;
+            address = req.body.address,
+            adharNumber = req.body.adharNumber;
+        pool.query("INSERT INTO userData(phonenumber, usertype, name, password, address, adharnumber) VALUES (" + phoneNumber + ", 0, " + name + ", " + password + "," + address + "," + adharNumber + ");", (err, res) => {
+            console.log(err, res);
+            //pool.end();
+        });
     });
 
 
